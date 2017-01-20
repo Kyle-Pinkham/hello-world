1dc880c7d5c8ef8c11cd4bd6581677b9ef0a2c75
Unknown <kyledpinkham@Gmail.com>
2017-01-19 20:14:49 -0700

    adding new "x" variable to the document creator

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,16 +9,13 @@ namespace ConsoleApplication1
 {
    class Program
     {
-        //public StreamWriter (
-        //string path,
-        //  bool append );
-
         static void Main(string[] args)
         {
             string path = @"D:\\";
             string fileName = "text.txt";
             string textToAdd = "This is a test file for our purposes";
-            
+
+            string text2Add = (x);
 
             using (StreamWriter writer = File.CreateText(path+fileName))
             {
